e344cb8b8def5dc10d95c67a92e9d54eb161a266
klr7696 <kientegalaurent11@gmail.com>
2021-10-30 11:58:35 +0000

    test en cours

---
+++ src/Entity/TestTable.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\TestTableRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=TestTableRepository::class)
+ * @ApiResource()
+ */
+class TestTable
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $entier;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $reel;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $date;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEntier(): ?int
+    {
+        return $this->entier;
+    }
+
+    public function setEntier(int $entier): self
+    {
+        $this->entier = $entier;
+
+        return $this;
+    }
+
+    public function getReel(): ?float
+    {
+        return $this->reel;
+    }
+
+    public function setReel(float $reel): self
+    {
+        $this->reel = $reel;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+}
